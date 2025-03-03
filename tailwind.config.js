ff97eba71e19495c397ecf38f90ab5277b19b7e1
Kashif Kazmi <kashifkazmi1412@gmail.com>
2025-03-03 14:27:40 +0500

    ads-parallel

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0074FD',
+                secondary: '#111D5E',
+                blue:'#00BCD4',
+                blue2:'#1e3a8a',
+                gray:"#212830"
+            }
+        }
+    }
+};
